c67cde67c830b8fc787eaf7a7d090e947113c00e
smRamiro <ramiro_sando@outlook.com>
2020-05-21 09:58:34 -0300

    Cleaner code

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,31 +1,10 @@
-let gameboardTiles = document.querySelectorAll(".gameboard-tiles");
-
-let gameBoardModule = (function(){
-    let gameboardArray = [];
-
-    return {gameboardArray};
-})();
-
-const players = (name, symbol) => {
-    const playerName = name;
-    const playerSymbol = symbol;
-    return{playerName, playerSymbol}
-}
-
-let player1 = players("Player 1", "x");
-let player2 = players("Player 2", "o");
-
-let isPlayer1Turn = true;
-
-function renderToTiles(){
+function giveGameboardTilesEventListeners(){
     for (let i = 0; i < gameboardTiles.length; i++) {
-        gameboardTiles[i].textContent = gameBoardModule.gameboardArray[i]; 
+        gameboardTiles[i].addEventListener("click", playerChoosesTile);
     }
 }
 
-
 function playerChoosesTile(e){
-
     const selectedTileId = e.target.getAttribute("id");
 
     if (isPlayer1Turn && selectedTileId != "alreadyDrawTile") {
@@ -41,6 +20,20 @@ function playerChoosesTile(e){
     }
 }
 
-for (let i = 0; i < gameboardTiles.length; i++) {
-    gameboardTiles[i].addEventListener("click", playerChoosesTile);
+let gameBoardModule = (function(){
+    let gameboardArray = [];
+    return {gameboardArray};
+})();
+
+const players = (name, symbol) => {
+    const playerName = name;
+    const playerSymbol = symbol;
+    return{playerName, playerSymbol}
 }
+
+let gameboardTiles = document.querySelectorAll(".gameboard-tiles");
+let player1 = players("Player 1", "x");
+let player2 = players("Player 2", "o");
+let isPlayer1Turn = true;
+
+giveGameboardTilesEventListeners();
