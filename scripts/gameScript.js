543c7c4650e5bb7d002c2a88cd6aa11e1eb22d89
smRamiro <ramiro_sando@outlook.com>
2020-05-21 09:39:34 -0300

    Turns logic and locked tiles logic are done

---
+++ scripts/gameScript.js
@@ -0,0 +1,46 @@
+let gameboardTiles = document.querySelectorAll(".gameboard-tiles");
+
+let gameBoardModule = (function(){
+    let gameboardArray = [];
+
+    return {gameboardArray};
+})();
+
+const players = (name, symbol) => {
+    const playerName = name;
+    const playerSymbol = symbol;
+    return{playerName, playerSymbol}
+}
+
+let player1 = players("Player 1", "x");
+let player2 = players("Player 2", "o");
+
+let isPlayer1Turn = true;
+
+function renderToTiles(){
+    for (let i = 0; i < gameboardTiles.length; i++) {
+        gameboardTiles[i].textContent = gameBoardModule.gameboardArray[i]; 
+    }
+}
+
+
+function playerChoosesTile(e){
+
+    const selectedTileId = e.target.getAttribute("id");
+
+    if (isPlayer1Turn && selectedTileId != "alreadyDrawTile") {
+        gameBoardModule.gameboardArray.push(player1.playerSymbol);
+        e.target.textContent = gameBoardModule.gameboardArray[gameBoardModule.gameboardArray.length - 1];
+        e.target.setAttribute("id", "alreadyDrawTile");
+        isPlayer1Turn = false;
+    }else if (!isPlayer1Turn && selectedTileId != "alreadyDrawTile"){
+        gameBoardModule.gameboardArray.push(player2.playerSymbol);
+        e.target.textContent = gameBoardModule.gameboardArray[gameBoardModule.gameboardArray.length - 1];
+        e.target.setAttribute("id", "alreadyDrawTile");
+        isPlayer1Turn = true;
+    }
+}
+
+for (let i = 0; i < gameboardTiles.length; i++) {
+    gameboardTiles[i].addEventListener("click", playerChoosesTile);
+}
