66ffa59ea62a230cdbadfa11d717143797dce0ab
smRamiro <ramiro_sando@outlook.com>
2020-05-23 19:47:44 -0300

    Finished

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -4,16 +4,23 @@ function giveGameboardTilesEventListeners(){
     }
 }
 
-function playerChoosesTile(e){
-    const selectedTileId = e.target.getAttribute("id");
+function giveResetButtonEventListener(){
+    const RESTARTBUTTON = document.getElementById("reset-button");
+    RESTARTBUTTON.addEventListener("click", restartGame);
+}
 
-    if (isPlayer1Turn && selectedTileId != "alreadyDrawnTile") {
-        gameBoardModule.gameboardArray.push(player1.playerSymbol);
+function playerChoosesTile(e){
+    const SELECTEDTILEID = e.target.getAttribute("id");
+    
+    changePlayerTextColorIfItsHisTurn(e);
+    
+    if (isPlayer1Turn && SELECTEDTILEID != "alreadyDrawnTile") {
+        gameBoardModule.gameboardArray.push(player1.PLAYERSYMBOL);
         e.target.textContent = gameBoardModule.gameboardArray[gameBoardModule.gameboardArray.length - 1];
         e.target.setAttribute("id", "alreadyDrawnTile");
         isPlayer1Turn = false;
-    }else if (!isPlayer1Turn && selectedTileId != "alreadyDrawnTile"){
-        gameBoardModule.gameboardArray.push(player2.playerSymbol);
+    }else if (!isPlayer1Turn && SELECTEDTILEID != "alreadyDrawnTile"){
+        gameBoardModule.gameboardArray.push(player2.PLAYERSYMBOL);
         e.target.textContent = gameBoardModule.gameboardArray[gameBoardModule.gameboardArray.length - 1];
         e.target.setAttribute("id", "alreadyDrawnTile");
         isPlayer1Turn = true;
@@ -22,71 +29,120 @@ function playerChoosesTile(e){
     checkIfSomeoneWinned(e);
 }
 
+function changePlayerTextColorIfItsHisTurn(e){
+    const PLAYER1TEXT = document.getElementById("p1");
+    const PLAYER2TEXT = document.getElementById("p2");
+
+    const CURRENTTILE = e.target;
+    console.log(CURRENTTILE.getAttribute("id"));
+    
+    if (isPlayer1Turn && !someoneWinned && !CURRENTTILE.getAttribute("id")){
+        PLAYER1TEXT.style.transform = "scale(1)";
+        PLAYER1TEXT.style.fontWeight = "normal";
+        PLAYER2TEXT.style.transform = "scale(1.025)";
+        PLAYER2TEXT.style.fontWeight = "bold";
+
+    }else if(!isPlayer1Turn && !someoneWinned && !CURRENTTILE.getAttribute("id")){
+        PLAYER2TEXT.style.transform = "scale(1)";
+        PLAYER2TEXT.style.fontWeight = "normal";
+        PLAYER1TEXT.style.transform = "scale(1.025)";
+        PLAYER1TEXT.style.fontWeight = "bold";
+    }
+}
+
+function checkIfItsADraw(){
+    if(gameBoardModule.gameboardArray.length == 9 && !someoneWinned && !someoneWinned){
+        alert("It's a draw!");
+        gameIsOver();
+    }
+}
+
 function checkIfSomeoneWinned(e){
 
     for (let tile = 0; tile < gameboardTiles.length; tile++) {
-        if(tile <= 2){//Vertical row starting from top
-            if (gameboardTiles[tile].textContent == player1.playerSymbol){// Player is starting the row
+        if(tile <= 2){//Conditional for row starting from top
+            if (gameboardTiles[tile].textContent == player1.PLAYERSYMBOL){// Player is starting the row
 
-                if (gameboardTiles[tile+3].textContent == player1.playerSymbol && gameboardTiles[tile+6].textContent == player1.playerSymbol && !someoneWinned){ //there's a vertical row
+                if (gameboardTiles[tile+3].textContent == player1.PLAYERSYMBOL && gameboardTiles[tile+6].textContent == player1.PLAYERSYMBOL && !someoneWinned){ //there's a vertical row
                     alert("Player 1 is the winner!");
-                    someoneWinned = true;
                     gameIsOver();
 
-                }else if (tile == 2){ //Conditional for horizontal row starting from the superior right
+                }else if (tile == 2){ //Conditional for row starting from the superior right
                     
-                    if (gameboardTiles[tile].textContent == player1.playerSymbol){ //Player 1
-                        if (gameboardTiles[tile-1].textContent == player1.playerSymbol && gameboardTiles[tile-2].textContent == player1.playerSymbol && !someoneWinned) {
-                            alert("Player 1 wins -horizontal-");
-                            someoneWinned = true;
+                    if (gameboardTiles[tile].textContent == player1.PLAYERSYMBOL){ //Player 1
+                        if (gameboardTiles[tile-1].textContent == player1.PLAYERSYMBOL && gameboardTiles[tile-2].textContent == player1.PLAYERSYMBOL && !someoneWinned) {
+                            alert("Player 1 wins");
+                            gameIsOver();
+                        } else if(gameboardTiles[tile+2].textContent == player1.PLAYERSYMBOL && gameboardTiles[tile+4].textContent == player1.PLAYERSYMBOL && !someoneWinned){
+                            alert("Player 1 wins");
                             gameIsOver();
                         }
                     }
-
+                }else if (tile == 0){//row starting from the first tile
+                    if(gameboardTiles[tile+4].textContent == player1.PLAYERSYMBOL && gameboardTiles[tile+8].textContent == player1.PLAYERSYMBOL && !someoneWinned){
+                        alert("Player 1 wins");
+                        gameIsOver();
+                    }
                 }
-            }else if (gameboardTiles[tile].textContent == player2.playerSymbol) {//Player 2
+            }else if (gameboardTiles[tile].textContent == player2.PLAYERSYMBOL) {//Player 2
 
-                if (gameboardTiles[tile+3].textContent == player2.playerSymbol && gameboardTiles[tile+6].textContent == player2.playerSymbol && !someoneWinned){
+                if (gameboardTiles[tile+3].textContent == player2.PLAYERSYMBOL && gameboardTiles[tile+6].textContent == player2.PLAYERSYMBOL && !someoneWinned){
                     alert("Player 2 is the winner!");
-                    someoneWinned = true;
                     gameIsOver();
                     
                 }else if (tile == 2){
 
-                    if (gameboardTiles[tile].textContent == player2.playerSymbol){ //Player 2
-                        if (gameboardTiles[tile-1].textContent == player2.playerSymbol && gameboardTiles[tile-2].textContent == player2.playerSymbol && !someoneWinned) {
-                            alert("Player 2 wins -horizontal-");
-                            someoneWinned = true;
+                    if (gameboardTiles[tile].textContent == player2.PLAYERSYMBOL){ //Player 2
+                        if (gameboardTiles[tile-1].textContent == player2.PLAYERSYMBOL && gameboardTiles[tile-2].textContent == player2.PLAYERSYMBOL && !someoneWinned) {
+                            alert("Player 2 wins");
+                            gameIsOver();
+                        } else if(gameboardTiles[tile+2].textContent == player2.PLAYERSYMBOL && gameboardTiles[tile+4].textContent == player2.PLAYERSYMBOL && !someoneWinned){
+                            alert("Player 2 wins");
                             gameIsOver();
                         }
                     }
-
+                }else if (tile == 0){//row starting from the first tile
+                    if(gameboardTiles[tile+4].textContent == player2.PLAYERSYMBOL && gameboardTiles[tile+8].textContent == player2.PLAYERSYMBOL && !someoneWinned){
+                        alert("Player 2 wins");
+                        gameIsOver();
+                    }
                 }
             }
 
         }else if (tile == 0 || tile == 3 || tile == 6) {//Horizontal row starting from left
 
-            if (gameboardTiles[tile].textContent == player1.playerSymbol) { //Player 1
+            if (gameboardTiles[tile].textContent == player1.PLAYERSYMBOL) { //Player 1
 
-                if (gameboardTiles[tile+1].textContent == player1.playerSymbol && gameboardTiles[tile+2].textContent == player1.playerSymbol && !someoneWinned){
+                if (gameboardTiles[tile+1].textContent == player1.PLAYERSYMBOL && gameboardTiles[tile+2].textContent == player1.PLAYERSYMBOL && !someoneWinned){
                     alert("Player 1 is the winner! -horizontal-");
-                    someoneWinned = true;
                     gameIsOver();
                 }
 
-            }else if (gameboardTiles[tile].textContent == player2.playerSymbol) {
+            }else if (gameboardTiles[tile].textContent == player2.PLAYERSYMBOL) {
 
-                if (gameboardTiles[tile+1].textContent == player2.playerSymbol && gameboardTiles[tile+2].textContent == player2.playerSymbol && !someoneWinned){
+                if (gameboardTiles[tile+1].textContent == player2.PLAYERSYMBOL && gameboardTiles[tile+2].textContent == player2.PLAYERSYMBOL && !someoneWinned){
                     alert("Player 2 is the winner! -horizontal-");
-                    someoneWinned = true;
                     gameIsOver();
                 }
-                
             }
         }
     }
+    checkIfItsADraw();
 }
+
+function restartGame(){
+    someoneWinned = false;
+    isPlayer1Turn = true;
+    for (let i = 0; i < gameboardTiles.length; i++) {
+        gameBoardModule.gameboardArray.pop();
+        gameboardTiles[i].textContent = "";
+        gameboardTiles[i].removeAttribute("id");
+    }
+}
+
 function gameIsOver(){
+    someoneWinned = true;
+
     for (let i = 0; i < gameboardTiles.length; i++) {
         gameboardTiles[i].setAttribute("id", "alreadyDrawnTile");
     }
@@ -98,9 +154,9 @@ let gameBoardModule = (function(){
 })();
 
 const players = (name, symbol) => {
-    const playerName = name;
-    const playerSymbol = symbol;
-    return{playerName, playerSymbol}
+    const PLAYERNAME = name;
+    const PLAYERSYMBOL = symbol;
+    return{PLAYERNAME, PLAYERSYMBOL}
 }
 
 let gameboardTiles = document.querySelectorAll(".gameboard-tiles");
@@ -108,4 +164,6 @@ let player1 = players("Player 1", "x");
 let player2 = players("Player 2", "o");
 let isPlayer1Turn = true;
 let someoneWinned = false;
+
 giveGameboardTilesEventListeners();
+giveResetButtonEventListener();
